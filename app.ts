651d8542a2d2aefdd38ecd7901f6e5744199fc12
Leonard Oduneye <leonardoduneye@outlook.com>
2023-05-09 10:13:20 +0100

    initial commit

---
+++ app.ts
@@ -0,0 +1,9 @@
+import express, { Application } from "express";
+import morgan from "morgan";
+
+import indexRoutes from './routes/index.routes';
+import postRoutes from './routes/post.routes'
+
+export class App {
+    
+}
